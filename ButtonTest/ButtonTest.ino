ab25179a4aefd91d3f71335eb0409c3da5dd4d0a
Ethan Mathew <ethmatt09@gmail.com>
2023-02-08 17:59:49 -0600

    Cleaned up my messy programming

--- ButtonTest/ButtonTest.ino
+++ ButtonTest/ButtonTest.ino
@@ -1,24 +1,73 @@
-int buttonPin = 1;
+const int buttonInput1 = 1;
+const int buttonInput2 = 2;
+const int buttonInput3 = 3;
+const int buttonInput4 = 4;
+const int buttonInput5 = 5;
+const int buttonInput6 = 6;
+const int buttonInput7 = 7;
+const int buttonInput8 = 8;
+const int buttonInput9 = 9;
 
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
+int buttonList[9] = {buttonInput1, buttonInput2, buttonInput3, 
+                      buttonInput4, buttonInput5, buttonInput6, 
+                      buttonInput7, buttonInput8, buttonInput9};
+
+int lightOutput = 13;
+int button1 = 0;
+int button2 = 0;
+int button3 = 0;
+int button4 = 0;
+int button5 = 0;
+int button6 = 0;
+int button7 = 0;
+int button8 = 0;
+int button9 = 0;
+
+void setup() { 
+  // pu n  t your setup code here, to run once:
+    int j;
+    for (j=0; j < 9; j++) {
+      pinMode(buttonList[j], INPUT);
+      digitalWrite(buttonList[j], HIGH);
+    }
+    Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  int current = analogRead(buttonPin);
-
-  Serial.println (current);
-
-  if  (current < 100) 
-  {
-
+  int w;
+  for (w=0; w<9; w++) {
+    "button"+String(w) = digitalRead(buttonList[w]); 
   }
-
-  else if  (current < 200) 
-  {
   
-  }
-
+    if  (button1 == HIGH) {
+        Serial.println("Button 1 On");
+      } 
+    else if  (button2 == HIGH) {
+        Serial.println("Button 25 On");
+      } 
+    else if  (button3 == HIGH) {
+        Serial.println("Button 3 On");
+      }
+    else if  (button4 == HIGH) {
+        Serial.println("Button 4 On");
+      }
+    else if  (button5 == HIGH) {
+        Serial.println("Button 5 On");
+      }
+    else if  (button6 == HIGH) {
+        Serial.println("Button 6 On");
+      } 
+    else if  (button7 == HIGH) {
+        Serial.println("Button 7 On");
+      }
+    else if  (button8 == HIGH) {
+        Serial.println("Button 8 On");
+      }
+    else if  (button9 == HIGH) {
+        Serial.println("Button 9 On");
+      }
+    else {
+        Serial.println("Off ");
+        
+    }
 }
--- LightTest/LightTest.ino
+++ LightTest/LightTest.ino
@@ -1,106 +1,57 @@
-const int button1Input = 1;
-const int button2Input = 2;
-const int button3Input = 3;
-const int button4Input = 4;
-const int button5Input = 5;
-const int button6Input = 6;
-const int button7Input = 7;
-const int button8Input = 8;
-const int button9Input = 9;
-int lightOutput = 13;
-int button1 = 0;
-int button2 = 0;
-int button3 = 0;
-int button4 = 0;
-int button5 = 0;
-int button6 = 0;
-int button7 = 0;
-int button8 = 0;
-int button9 = 0;
+#include "LPD8806.h"
+#include "SPI.h" // Comment out this line if using Trinket or Gemma
+#ifdef __AVR_ATtiny85__
+ #include <avr/power.h>
+#endif
+
+int dataPin  = 11;
+int clockPin = 13;
+
+LPD8806 strip = LPD8806(31, dataPin, clockPin);
 
 void setup() {
-  // pu n  t your setup code here, to run once:
-    pinMode(lightOutput, OUTPUT);
-  //button 1 init
-    pinMode(button1Input, INPUT);
-    digitalWrite(button1Input, HIGH);
-  //button 2 init
-    pinMode(button2Input, INPUT);
-    digitalWrite(button2Input, HIGH);
-  //button 3 init
-    pinMode(button3Input, INPUT);
-    digitalWrite(button3Input, HIGH);
-  //button 4 init
-    pinMode(button4Input, INPUT);
-    digitalWrite(button4Input, HIGH);
-  //button 5 init
-    pinMode(button5Input, INPUT);
-    digitalWrite(button5Input, HIGH);
-  //button 6 init
-    pinMode(button6Input, INPUT);
-    digitalWrite(button6Input, HIGH);
-  //button 7 init
-    pinMode(button1Input, INPUT);
-    digitalWrite(button1Input, HIGH);
-  //button 8 init
-    pinMode(button8Input, INPUT);
-    digitalWrite(button8Input, HIGH);
-  //button 9 init
-    pinMode(button9Input, INPUT);
-    digitalWrite(button9Input, HIGH);  
-    Serial.begin(9600);
+  // Start up the LED strip
+  strip.begin();
+
+  // Update the strip, to start they are all 'off'
+  strip.show();  
+  
+  Serial.begin(9600);
 }
 
 void loop() {
-   button1 = digitalRead(button1Input);
-   button2 = digitalRead(button2Input);
-   button3 = digitalRead(button3Input);
-   button4 = digitalRead(button4Input);
-   button5 = digitalRead(button5Input);
-   button6 = digitalRead(button6Input);
-   button7 = digitalRead(button7Input);
-   button8 = digitalRead(button8Input);
-   button9 = digitalRead(button9Input);
   
-    if  (button1 == HIGH) {
-        digitalWrite(lightOutput, HIGH);
-        Serial.println("Button 1 On");
-      } 
-    else if  (button2 == HIGH) {
-        digitalWrite(lightOutput, HIGH);
-        Serial.println("Button 2 On");
-      } 
-    else if  (button3 == HIGH) {
-        digitalWrite(lightOutput, HIGH);
-        Serial.println("Button 3 On");
-      }
-    else if  (button4 == HIGH) {
-        digitalWrite(lightOutput, HIGH);
-        Serial.println("Button 4 On");
-      }
-    else if  (button5 == HIGH) {
-        digitalWrite(lightOutput, HIGH);
-        Serial.println("Button 5 On");
-      }
-    else if  (button6 == HIGH) {
-        digitalWrite(lightOutput, HIGH);
-        Serial.println("Button 6 On");
-      } 
-    else if  (button7 == HIGH) {
-        digitalWrite(lightOutput, HIGH);
-        Serial.println("Button 7 On");
-      }
-    else if  (button8 == HIGH) {
-        digitalWrite(lightOutput, HIGH);
-        Serial.println("Button 8 On");
-      }
-    else if  (button9 == HIGH) {
-        digitalWrite(lightOutput, HIGH);
-        Serial.println("Button 9 On");
-      }
-    else {
-        digitalWrite(lightOutput, LOW);
-        Serial.println("Off ");
-        
-    }
+  colorChase(strip.Color(127,   0,   0), 50); // Red
+  delay(10);
+  TurnOff();
+  delay(10);
+  colorChase(strip.Color(127, 127,   0), 50); // Yellow
+  delay(10);
+  TurnOff();
+  delay(10);
+  
+}
+
+void colorChase(uint32_t c, uint8_t wait) {
+  int i;
+
+  // Start by turning all pixels off:
+  for(i=0; i<strip.numPixels(); i++) strip.setPixelColor(i, 0);
+
+  // Then display one pixel at a time:
+  for(i=0; i<strip.numPixels(); i++) {
+    strip.setPixelColor(i, c); // Set new pixel 'on'
+  }
+
+  strip.show(); // Refresh to turn off last pixel
+}
+
+void TurnOff() {
+  int w;
+  for(w=0; w<strip.numPixels(); w++) {
+    strip.setPixelColor(w, 0); // Set new pixel 'off'
+    delay(10);
+  }
+    
+strip.show();
 }
